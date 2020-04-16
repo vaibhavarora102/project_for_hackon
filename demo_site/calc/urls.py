a0bdfbc95c44e626e5ee84ab72deec573662ed9a
devel-jot <jotroop2001@gmail.com>
2020-04-16 13:59:37 -0400

    websit  demo
    just download demo sit and run command "python manage.py runserver"
    need django to be installed in python

---
+++ demo_site/calc/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.home, name='login'),
+    path('login_sub',views.login_sub,name='login_sub'),
+    path('disclamer',views.disclamer, name='disclamer'),
+    path('form_sub',views.form_sub, name='form_sub'),
+    path('add',views.add,name='add'),
+]
+ 
